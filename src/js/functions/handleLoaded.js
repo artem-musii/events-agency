ddf4b68a824cae15aa773eb237187027fd36101a
Artem Musii <musiy.artem@gmail.com>
2024-05-04 11:06:21 +0200

    fix

--- src/js/functions/handleLoaded.js
+++ src/js/functions/handleLoaded.js
@@ -1,4 +1,4 @@
 export const handleLoaded = () => {
   document.querySelector('.loader').style.display = 'none';
-  document.body.classList.overflow = 'auto';
+  document.body.style.overflow = 'auto';
 };
